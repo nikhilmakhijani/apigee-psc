2679d052d1aec2552b431d54bd51af664651c9a1
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2023-08-09 20:22:18 +0530

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -1,21 +1,114 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Divider,
+  Icon,
+  VStack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { RiAddfill } from 'react-icons/ri';
+import {
+  isToday,
+  isYesterday,
+  isWithinInterval,
+  sub,
+} from 'date-fns';
+import { Session } from '../../common/Api'; // Import your Session type
+
+interface SessionsSidebarProps {
+  sessions: Session[];
+  currentSession: Session | null;
+  setCurrentSession: React.Dispatch<React.SetStateAction<Session | null>>;
+  openCreateSession: () => void;
+}
+
+const SessionButton = ({
+  session,
+  isCurrentSession,
+  onClick,
+  onEdit,
+  onDelete,
+}: SessionButtonProps) => {
+  const tip = `started ${formatDistanceToNow(session.created, {
+    includeSeconds: true,
+  })} ago`;
 
   return (
     <Tooltip label={tip}>
       <ButtonGroup isAttached variant={isCurrentSession ? 'solid' : 'ghost'} w="100%">
-        <IconButton aria-label="Edit" icon={<RiPencilLine />} onClick={handleEditClick} />
-        {isEditing ? (
-          <>
-            <Input value={editedName} onChange={(e) => setEditedName(e.target.value)} />
-            <IconButton aria-label="Save" icon={<FaCheck />} onClick={handleSaveClick} />
-            <IconButton aria-label="Cancel" icon={<FaTimes />} onClick={handleCancelClick} />
-          </>
-        ) : (
-          <Button variant={isCurrentSession ? 'solid' : 'ghost'} w="100%" onClick={onClick}>
-            {session.name}
-          </Button>
-        )}
-        <IconButton aria-label="Delete" icon={<RiDeleteBin6Line />} onClick={onDelete} />
+        <Button variant={isCurrentSession ? 'solid' : 'ghost'} onClick={onClick} w="100%">
+          {session.name}
+        </Button>
+        <Button onClick={onDelete} colorScheme="red" leftIcon={<RiDeleteBin6Line />}>
+          Delete
+        </Button>
       </ButtonGroup>
     </Tooltip>
   );
 };
+
+const SessionsSidebar = (props: SessionsSidebarProps) => {
+  const headerCol = useColorModeValue('gray.600', 'gray.400');
+  const { currentSession, setCurrentSession, openCreateSession } = props;
+
+  const [sessions, setSessions] = useState(props.sessions);
+
+  const handleSessionEdit = (sessionId: string, editedName: string) => {
+    const updatedSessions = sessions.map((session) =>
+      session.id === sessionId ? { ...session, name: editedName } : session
+    );
+    setSessions(updatedSessions);
+  };
+
+  const handleSessionDelete = (sessionId: string) => {
+    const updatedSessions = sessions.filter((session) => session.id !== sessionId);
+    setSessions(updatedSessions);
+    if (currentSession?.id === sessionId) {
+      setCurrentSession(null);
+    }
+  };
+
+  // Define your session intervals and sessionMap here...
+
+  return (
+    <VStack align="left">
+      <Button
+        colorScheme="gray"
+        variant="ghost"
+        onClick={openCreateSession}
+        w="97%"
+        my={3}
+        leftIcon={<Icon as={RiAddfill} />}
+      >
+        New Session
+      </Button>
+      <Divider />
+
+      {Object.keys(sessionMap).map((title, mapIndex) => {
+        if (sessionMap[title].length === 0) {
+          return null;
+        }
+        return (
+          <React.Fragment key={mapIndex}>
+            <Text fontWeight="semibold" color={headerCol} mt={2}>
+              {title}
+            </Text>
+            {sessionMap[title].map((session, titleIndex) => (
+              <SessionButton
+                key={titleIndex}
+                session={session}
+                isCurrentSession={currentSession === session}
+                onClick={() => setCurrentSession(session)}
+                onEdit={(editedName) => handleSessionEdit(session.id, editedName)}
+                onDelete={() => handleSessionDelete(session.id)}
+              />
+            ))}
+          </React.Fragment>
+        );
+      })}
+    </VStack>
+  );
+};
+
+export default SessionsSidebar;
